b1f15334faf3f58167ab21cd75e2d0b907518671
Moses Palmér <mosespalmer@gmail.com>
2012-08-12 15:59:31 +0200

    Simplified adding output formats to etld-parser.py

--- tools/etld-parser.py
+++ tools/etld-parser.py
@@ -21,68 +21,6 @@ import os
 import re
 
 
-HEADER_FORMAT = '''
-/* Copyright 2012 Moses Palmér
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
-#ifndef PUBSUF_ETLDS_H
-#define PUBSUF_ETLDS_H
-
-/**
- * The maximum depth of any of the rules.
- */
-#define ETLDS_MAX_DEPTH %d
-
-/**
- * The list of eTLD name rules.
- *
- * This is a NULL terminated list with ETLDS_MAX_DEPTH NULL terminated lists
- * of strings.
- *
- * The top-level lists are sorted with depth descending.
- *
- * The lower-level lists are sorted alphabetically and partitioned so that the
- * first part is exceptions if any, the second part is wilcards if any and the
- * third part is plain rules.
- */
-extern const char **pubsuf_etlds[ETLDS_MAX_DEPTH + 1];
-
-#endif
-'''[1:]
-
-SOURCE_FORMAT = '''
-#include <stdlib.h>
-
-#include "etlds.h"
-
-%s
-
-const char **pubsuf_etlds[ETLDS_MAX_DEPTH + 1] = {
-%s
-    NULL};
-'''[1:]
-
-DEPTH_FORMAT = '''
-static const char *depth%d[] = {
-%s
-    NULL};
-'''[1:]
-
-
 def read_etlds(input_file):
     """
     Reads all effective TLDs found in input file.
@@ -127,17 +65,82 @@ def load_etld_database(database):
         for i in range(1, max(e.count('.') for e in etlds) + 1)]
 
 
-def main(database, header_name, source_name):
-    # Load the TLD database
-    depths = load_etld_database(database)
+def c_source(depths, c_source_h, c_source_c, **kwargs):
+    """
+    Converts an effective TLD database to a header and source file.
 
-    # Get the timestamp of the database
-    st = os.stat(database)
+    @param depths
+        The effective TLD database.
+    @param c_source_h
+        The name of the header file to write.
+    @param c_source_c
+        Th name of the source file to write.
+    @return a list of generated file names
+    """
+    HEADER_FORMAT = '''
+/* Copyright 2012 Moses Palmér
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef PUBSUF_ETLDS_H
+#define PUBSUF_ETLDS_H
+
+/**
+ * The maximum depth of any of the rules.
+ */
+#define ETLDS_MAX_DEPTH %d
+
+/**
+ * The list of eTLD name rules.
+ *
+ * This is a NULL terminated list with ETLDS_MAX_DEPTH NULL terminated lists
+ * of strings.
+ *
+ * The top-level lists are sorted with depth descending.
+ *
+ * The lower-level lists are sorted alphabetically and partitioned so that the
+ * first part is exceptions if any, the second part is wilcards if any and the
+ * third part is plain rules.
+ */
+extern const char **pubsuf_etlds[ETLDS_MAX_DEPTH + 1];
+
+#endif
+'''[1:]
+
+    SOURCE_FORMAT = '''
+#include <stdlib.h>
+
+#include "etlds.h"
+
+%s
+
+const char **pubsuf_etlds[ETLDS_MAX_DEPTH + 1] = {
+%s
+    NULL};
+'''[1:]
+
+    DEPTH_FORMAT = '''
+static const char *depth%d[] = {
+%s
+    NULL};
+'''[1:]
 
     # Write the header file
-    with open(header_name, 'w') as f:
+    with open(c_source_h, 'w') as f:
         f.write(HEADER_FORMAT % len(depths))
-    os.utime(header_name, (st.st_atime, st.st_mtime))
 
     # To make sure hex escaping is correct, we need to write hex encoding
     # separately
@@ -152,10 +155,25 @@ def main(database, header_name, source_name):
             for d in range(len(depths)))
     depth_listing = '\n'.join('    depth%d,' % (d + 1)
         for d in range(len(depths)))
-    with open(source_name, 'w') as f:
+    with open(c_source_c, 'w') as f:
         f.write(SOURCE_FORMAT % (depth_definitions, depth_listing))
 
-    return 0;
+    return (c_source_h, c_source_c)
+
+
+# Intialise the output type map
+output_types = {
+    'c-source': c_source}
+
+
+def main(output_type, database, **kwargs):
+    # Load the TLD database
+    depths = load_etld_database(database)
+
+    # Get the timestamp of the database
+    st = os.stat(database)
+    for output_file in output_types[output_type](depths, **kwargs):
+        os.utime(output_file, (st.st_atime, st.st_mtime))
 
 if __name__ == '__main__':
     import sys
@@ -163,10 +181,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description = 'Parses an eTLD database and outputs formatted data.');
-    parser.add_argument('--header-name',
+    parser.add_argument('--c-source-h',
         help = 'The header to output')
-    parser.add_argument('--source-name',
+    parser.add_argument('--c-source-c',
         help = 'The C source file to output')
+
+    parser.add_argument('output_type',
+        help = 'The type of output to generate.', choices = output_types.keys())
     parser.add_argument('database',
         help = 'The database file to read.')
 
